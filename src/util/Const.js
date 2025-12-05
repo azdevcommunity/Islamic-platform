502d862574cd63f21b960427675dfb1818714ccd
yusifhasanov <yusif.hasanov17@gmail.com>
2025-12-05 14:05:45 +0400

    change bank data

--- src/util/Const.js
+++ src/util/Const.js
@@ -2,21 +2,21 @@ export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
 
 export const bankData = [
-    {
-        id: 1,
-        bankName: "Leo Bank",
-        cardCode: "5411 2498 0153 0416"
-    },
-    {
-        id: 2,
-        bankName: "ABB Bank",
-        cardCode: "5522 0993 6029 9366"
-    },
     // {
-    //     id: 3,
-    //     bankName: "Kapital Bank",
-    //     cardCode: "4169 7388 7530 8813"
+    //     id: 1,
+    //     bankName: "Leo Bank",
+    //     cardCode: "5411 2498 0153 0416"
     // },
+    // {
+    //     id: 2,
+    //     bankName: "ABB Bank",
+    //     cardCode: "5522 0993 6029 9366"
+    // },
+    {
+        id: 3,
+        bankName: "Kapital Bank",
+        cardCode: "4169 7388 9295 8806"
+    },
 
 ]
 
